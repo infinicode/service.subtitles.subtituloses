e155e831e12a124a87396ed01e448f3a5f9e19d7
infinicode <code@infinicode.org>
2014-02-08 19:42:32 +0100

    Added utils file

---
+++ resources/lib/utils.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+languages = {
+	"Espanol (Espana)": ("es", "ESP", 1),
+	"Espanol (Latinoamerica)": ("es", "ESP", 2),
+	"English": ("en", "ENG", 2),
+	"French": ("fr", "FRE", 2),
+	"Italian": ("it", "ITA", 2),
+	"Unknown": ("-", "???", 3),
+}
